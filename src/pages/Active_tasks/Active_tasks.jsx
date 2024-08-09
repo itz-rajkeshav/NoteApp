abbc1de40df3596333eb998c96316c1883a868e7
keshav <itsrajkeshav@gmail.com>
2024-08-09 17:15:25 +0530

    update function

--- src/pages/Active_tasks/Active_tasks.jsx
+++ src/pages/Active_tasks/Active_tasks.jsx
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 function Active_tasks() {
-  
-    const [showslide, setshowslide] = useState(false);
-    const navigate = useNavigate();
-  
-    return (
-      <div>
-        <div className='bg-zinc-700 w-full h-dvh'>
+  const [showslide, setshowslide] = useState(false);
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <div className="bg-zinc-700 w-full h-dvh">
         <button
           className="text-zinc-100 absolute ml-4 mt-7 z-50"
           onClick={() => setshowslide(!showslide)}
@@ -21,7 +20,11 @@ function Active_tasks() {
             xmlns="http://www.w3.org/2000/svg"
           >
             <g id="SVGRepo_bgCarrier" strokeWidth="0" />
-            <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" />
+            <g
+              id="SVGRepo_tracerCarrier"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
             <g id="SVGRepo_iconCarrier">
               <path
                 d="M4 6H20M4 12H20M4 18H20"
@@ -38,19 +41,19 @@ function Active_tasks() {
             <div className="mt-20 flex flex-col space-y-6">
               <button
                 className="w-72 bg-zinc-900 h-10 text-zinc-300 font-poppins hover:text-red-300"
-                onClick={() => navigate('/All_tasks')}
+                onClick={() => navigate("/All_tasks")}
               >
                 Entire Tasks
               </button>
               <button
                 className="w-72 bg-zinc-900 h-10 text-zinc-300 font-poppins hover:text-red-300"
-                onClick={() => navigate('/Active_tasks')}
+                onClick={() => navigate("/Active_tasks")}
               >
                 Active Tasks
               </button>
               <button
                 className="w-72 bg-zinc-900 h-10 text-zinc-300 font-poppins hover:text-red-300"
-                onClick={() => navigate('/Achieved_tasks')}
+                onClick={() => navigate("/Achieved_tasks")}
               >
                 Achieved Tasks
               </button>
@@ -62,11 +65,9 @@ function Active_tasks() {
             </footer>
           </div>
         )}
-        </div>
       </div>
-    )
-  
-  
+    </div>
+  );
 }
 
-export default Active_tasks
+export default Active_tasks;
